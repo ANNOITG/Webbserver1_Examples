faa7bf1a757a128f40f3f27b848e640972992b8e
ANNOITG <andre.norberglindhe@it-gymnasiet.se>
2014-01-17 10:11:41 +0100

    Exmaple with forms and JQuery
    Nothing moore.... :P

---
+++ Layout and JQuery/Scripts/JavaScript.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="jquery-2.0.3.min.js" />
+
+
+/*.menu ul li:hover {
+            background-color: rgba(255, 255, 255, 0.1);
+            margin-left: 20px;
+        }*/
+
+
+
+$(document).ready(function () {
+    $('.menu ul li').hover(function () {
+        $(this).stop().animate(
+            {
+            marginLeft: "20px",
+            }, 300);
+    }, function () {
+        $(this).stop().animate(
+            {
+            marginLeft: "0",
+            }, 100);
+    });
+});
